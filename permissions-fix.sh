50068bee6dcc35c2e410f450d97b6892e2f03b64
Binayak <binayakd86@gmail.com>
2024-09-03 23:14:45 +0800

    wip

--- permissions-fix.sh
+++ permissions-fix.sh
@@ -1,11 +1,8 @@
 #/bin/bash
 
 # runs this script if you are having permissions issues.
-# setup directories for local data
-mkdir -p ./local-data/downloaded-data
-mkdir -p ./local-data/minio-data
-mkdir -p ./local-data/postgres-data
 
-# change permission of workspace files 
+# change permission of workspace files and local-data folder 
 # to be accesable in the container
 chmod 777 -R ./workspace
+chmod 777 -R ./local-data
